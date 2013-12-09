87f4af75a064e6806d5eb9917862e7013dc98f12
Balamurugan <balasvn08@gmail.com>
2013-12-09 20:12:37 +0530

    cancel my account

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,7 @@ class LoginController < ApplicationController
 	layout :compute_layout
 
 	def index
-		redirect_to new_user_session_path # :controller => 'devise/sessions', :action => 'new'
+		#redirect_to new_user_session_path # :controller => 'devise/sessions', :action => 'new'
 		#user_session_path
 		#new_user_session_path
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   devise_for :users
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +50,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "login#index"
+    root :to => "login#index"
 
   # See how all your routes lay out with "rake routes"
 
